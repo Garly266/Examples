f587e6259ad521968cf9d13d78c25aafa70fb538
Garly <tolya-lisichkin@yandex.ru>
2022-12-05 15:15:29 +0400

    examples006_guru

---
+++ Examples/Examples006_GuRu/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 1;
+int b = 2;
+int c = 4;
+int d = 5;
+int e = 6;
+int f = 9;
+int g = 8;
+int h = 7;
+int v = 4;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+if (f > max) max = f;
+if (g > max) max = g;
+if (h > max) max = h;
+if (v > max) max = v;
+
+Console.Write("max = ");
+Console.WriteLine(max);
