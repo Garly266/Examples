3c75ec2a634f351b748ecc73fee7a38788895cdf
Garly <tolya-lisichkin@yandex.ru>
2022-12-05 14:36:50 +0400

    examples003_new random

--- Examples/Examples003_Summ/Program.cs
+++ Examples/Examples003_Summ/Program.cs
@@ -1,5 +1,8 @@
-﻿int numberA = 3;
-int numberB = 5;
-int Summ = 0;
-Summ = numberA + numberB;
-Console.WriteLine(Summ);
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+//int Summ = 0;
+//Summ = numberA + numberB;
+//Console.WriteLine(Summ);
+Console.WriteLine(numberA + numberB);
